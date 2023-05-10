22726174f6896170165fd9fe279512d81c715eea
Nicolas <walsergomez@gmail.com>
2023-05-10 14:30:22 -0600

    ci: navbar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
+const routesApp = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Social Networks",
+    url: "/social-networks",
+  },
+  {
+    name: "Counter +5",
+    url: "/counter/1",
+  },
+  {
+    name: "Counter +10",
+    url: "/counter/2",
+  },
+];
+
 export const NavBar = () => {
-  return <>NavBar</>;
+  return (
+    <>
+      <ul>
+        {routesApp.map((data) => (
+          <li key={data.url}>
+            <a href={data.url}>{data.name}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
---
+++ src/components/notfound/NotFound.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NotFound = () => {
+  return <h2 style={{ color: "red" }}>Not found</h2>;
+};
